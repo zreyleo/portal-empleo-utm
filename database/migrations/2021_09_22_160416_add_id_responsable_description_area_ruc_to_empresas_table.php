8e7618647906d9d21b0893a98814dbad991e2f63
Regynald Zambrano <zreyleo@gmail.com>
2021-09-22 17:41:57 -0500

    feat: new fields (ruc, descripcion, area, registrado_por) added to tbl_empresa on schema public on db ppp_sistema

---
+++ database/migrations/2021_09_22_160416_add_id_responsable_description_area_ruc_to_empresas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdResponsableDescriptionAreaRucToEmpresasTable extends Migration
+{
+    // protected $connection = 'DB_ppp_sistema_SCHEMA_portal_empleo';
+    // protected $connection = 'DB_ppp_sistema_SCHEMA_public';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('DB_ppp_sistema_SCHEMA_public')->table('tbl_empresa', function (Blueprint $table) {
+            $table->string('ruc')->unique()->nullable();
+            $table->text('descripcion')->nullable();
+            $table->unsignedBigInteger('area')->nullable();
+            $table->unsignedBigInteger('registrado_por')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('tbl_empresa');
+        Schema::connection('DB_ppp_sistema_SCHEMA_public')->table('tbl_empresa', function (Blueprint $table) {
+            $table->dropColumn(['ruc', 'descripcion', 'area', 'registrado_por']);
+        });
+    }
+}
